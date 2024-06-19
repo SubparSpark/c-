97ccc82a8598ee4ca9fde0b8283a098cfd4d4d89
Tzsolt <tozsola@gmail.com>
2024-06-19 20:25:41 +0200

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, NavbarComponent],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { BookingComponent } from './booking/booking.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    { path: 'dashboard', component: MainComponent },
+    { path: 'booking', component: BookingComponent },
+    { path: 'booking', component: BookingComponent },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },  
+    { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
